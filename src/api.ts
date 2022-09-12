9c8886b1b09df2ae329e823df57993229e02b9e1
OdiousCode <gorgande@gmail.com>
2022-09-12 06:16:20 +0200

    routing setup

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { logger } from './middleware';
+import gameRouter from './game.router';
+
+
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 
+// Logs endpoint action to console
 app.use(logger);
 
 app.listen(port, () => {
   return console.log(`API is listening at http://localhost:${port}`);
 });
+
+// Get all routes
+app.use("/api/games", gameRouter);
---
+++ src/game.router.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const gameRouter = express.Router();
+// get list
+gameRouter.get("/", );
+// post game with validation
+gameRouter.post("/", );
+// delete game with id
+gameRouter.delete("/:id", );
+// Update game with supplied id and validated body);
+gameRouter.put("/:id", ); 
+
+export default gameRouter;
