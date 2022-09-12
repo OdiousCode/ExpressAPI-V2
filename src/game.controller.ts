b2dc582870eeb66daa622ddb3e0ead4af7196ab4
OdiousCode <gorgande@gmail.com>
2022-09-12 08:47:14 +0200

    Added comments on functions

--- src/game.controller.ts
+++ src/game.controller.ts
@@ -8,6 +8,7 @@ export const getAllGames= (req: Request, res: Response) => {
     res.send(loadGamesJSON("gameList.json"));
 }
 
+// Finds a specific game by ID (parameter), returns 404 not found if no matching game, else returns the game.
 export const findGame = (req: Request, res: Response) => {
     const gameToFind = gamesArray.find((game) => game.id === parseInt(req.params.id));
     if (!gameToFind) 
@@ -20,6 +21,7 @@ export const findGame = (req: Request, res: Response) => {
     }
 }
 
+// Creates a new game with data from request, ID simply +1 on the arrays length (so if 2 games in list, next game is ID 3)
 export const createGame = (req: Request, res: Response) => {
     const gameToAdd = {
         id: gamesArray.length +1,
@@ -32,7 +34,7 @@ export const createGame = (req: Request, res: Response) => {
     res.status(200).send({alert: "Added a new game to the list of games."})
 };
 
-
+// Finds game by ID in url (parameter), removes it from the array with splice and rewrites the JSON file. Returns 404 if no game found, 200 if ok.
 export const deleteGame = (req: Request, res: Response) => {
     const gameToDelete = gamesArray.find((g) => g.id === parseInt(req.params.id));
     if(!gameToDelete) 
@@ -45,6 +47,7 @@ export const deleteGame = (req: Request, res: Response) => {
     res.status(200).send({alert: "The game with ID " + gameToDelete.id + " has been deleted."});
 };
 
+// Finds game by ID in url (parameter), overwrites the game at the found index with data supplied in body (validated with middleware). Returns 404 if bad ID in url, else removes previous entry and then saves the JSON and returns 200 ok.
 export const updateGame = (req: Request, res: Response) =>  {
     const gameToUpdate = gamesArray.find((g) => g.id === parseInt(req.params.id));
     if(!gameToUpdate) 
