384619595dbde5ff37514943e82cddf8302dfbea
OdiousCode <gorgande@gmail.com>
2022-09-12 07:02:17 +0200

    added validation to create game, seems to work

--- src/game.router.ts
+++ src/game.router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createGame, findGame, getAllGames } from './game.controller';
+import { gameModelValidation } from './middleware';
 
 const gameRouter = express.Router();
 // get list
@@ -7,7 +8,7 @@ gameRouter.get("/", getAllGames );
 // get specific game
 gameRouter.get("/:id", findGame );
 // post game with validation
-gameRouter.post("/", createGame);
+gameRouter.post("/", gameModelValidation ,createGame);
 // delete game with id
 gameRouter.delete("/:id", );
 // Update game with supplied id and validated body);
